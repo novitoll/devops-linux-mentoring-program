169bb8c72b6e5e6d7ff4447a8c80b16adde35168
Sabyrzhan <sabrtasbolatov@gmail.com>
2017-08-24 19:06:38 +0600

    AWS Auto-scaling group launch configuration - user data

---
+++ AWS/asg_launch_configuration_user_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+yum install -y php php-mysql httpd
+
+service httpd start
+
+touch /var/www/html/index.php
+
+cat >> /var/www/html/index.php << EOF
+<?php
+  \$servername = "rds-mysql-1.cz7l2q90uve5.us-east-1.rds.amazonaws.com";
+  \$username = "db_user";
+  \$password = "p@ssword";
+  // Create connection
+  \$conn = new mysqli(\$servername, \$username, \$password);
+  // Check connection
+  if (\$conn->connect_error) {
+      die("Connection failed: " . \$conn->connect_error);
+  }
+  echo "Connected successfully";
+?>
+EOF
+
+chmod 664 /var/www/html/index.php
+
+chown apache:apache /var/www/html/index.php
